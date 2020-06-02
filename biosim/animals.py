de32a909bd15fbc88113f555e3c11c9e1dfe20d9
Haris <Harisk96@gmail.com>
2020-06-02 15:57:30 +0200

    Started to make animal superclass

---
+++ biosim/animals.py
@@ -0,0 +1,10 @@
+
+from numba import jit
+import numpy as np
+
+class Animals:
+
+"""Superclass that represents an animal. Contains all the features for for creating the animals.
+ herbivores and carnivores are subclasses of this superclass."""
+
+    def 
