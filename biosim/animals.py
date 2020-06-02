3b3bf48ae8963e5675a8e2e687cade4d4d437f4f
isakfinnoy <isfi@nmbu.no>
2020-06-02 18:56:30 +0200

    added subclasses

--- biosim/animals.py
+++ biosim/animals.py
@@ -2,9 +2,31 @@
 from numba import jit
 import numpy as np
 
+__author__ = "Haris Karovic", "Isak Finnøy"
+__email__ = "harkarov@nmbu.no", "isfi@nmbu.no"
+
 class Animals:
+    """
+    Superclass that represents an animal. Contains all the features for for creating the animals.
+    herbivores and carnivores are subclasses of this superclass.
+    """
 
-"""Superclass that represents an animal. Contains all the features for for creating the animals.
- herbivores and carnivores are subclasses of this superclass."""
+class Herbivore(Animal):
+    """
+    Herbivore subclass. Herbivores can stay in all the landscape types except water,
+    i.e desert, highland and lowland, but will only be able to feed in cells with highland or
+    lowland as landscape types. It tries to eat an amount F of food each year, but the amount of
+    food it can eat depends on the amount of food in the cell. Herbivores eat in a random order.
+    """
 
-    def 
+class Carnivore(Animal):
+    """
+    Carnivore subclass. Carnivores can stay in all landscape types except water, i.e desert,
+    highland and lowland, and will be able to prey on herbivores in all the aforementioned
+    landscape types, though not on other carnivores. The carnivores prey on herbivores until one of
+    the following conditions are met:
+                                     1. It has eaten an amount of herbivores >= F
+                                     2. It has tries to kill every herbivore in the cell
+    The carnivores prey in order of their fitness, so that the fittest carnivore eats first. They
+    try to kill one herbivore at a time, trying to kill the herbivore with the lowest fitness first.
+    """
