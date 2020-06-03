a5dd08473efabea6de9cf4025579737843315c08
isakfinnoy <isfi@nmbu.no>
2020-06-03 14:11:25 +0200

    put parameters

--- biosim/animals.py
+++ biosim/animals.py
@@ -75,21 +75,9 @@ class Herbivore(Animal):
     food it can eat depends on the amount of food in the cell. Herbivores eat in a random order.
     """
 
-    w_birth = 8.0
-    sigma_birth = 1.5
-    beta = 0.9
-    eta = 0.05
-    a_half = 40.0
-    phi_age = 0.6
-    w_half = 10.0
-    phi_weight = 0.1
-    mu = 0.25
-    gamma = 0.2
-    zeta = 3.5
-    xi = 1.2
-    omega = 0.4
-    F = 10.0
-    DeltaPhiMax = ???
+    parameters = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9, 'eta': 0.05, 'a_half': 40.0,
+    'phi_age': 0.6, 'w_half': 10.0, 'phi_weight': 0.1, 'mu': 0.25, 'gamma': 0.2, 'zeta': 3.5,
+    'xi': 1.2, 'omega': 0.4, 'F': 10.0, 'DeltaPhiMax': None}
 
     def feed(self):
 
@@ -105,25 +93,13 @@ class Carnivore(Animal):
     try to kill one herbivore at a time, trying to kill the herbivore with the lowest fitness first.
     """
 
-    w_birth = 6.0
-    sigma_birth = 1.0
-    beta = 0.75
-    eta = 0.125
-    a_half = 4.0
-    phi_age = 0.3
-    w_half = 4.0
-    phi_weight = 0.4
-    mu = 0.4
-    gamma = 0.8
-    zeta = 3.5
-    xi = 1.1
-    omega = 0.8
-    F = 50.0
-    DeltaPhiMax = 10.0
+    parameters = {'w_birth': 6.0, 'sigma_birth': 1.0, 'beta': 0.75, 'eta': 0.125, 'a_half': 4.0,
+    'phi_age': 0.3, 'w_half': 4.0, 'phi_weight': 0.4, 'mu': 0.4, 'gamma': 0.8, 'zeta': 3.5,
+    'xi': 1.1, 'omega': 0.8, 'F': 50.0, 'DeltaPhiMax': 10.0}
 
 
     def slay(self):
         """
-        Function that determines wheter and when a carnivore will kill a herbivore and consume it.
+        Function that determines wether and when a carnivore will kill a herbivore and consume it.
         :return:
         """
