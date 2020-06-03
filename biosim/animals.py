e833666d074d1aa796c3d913bceb6903b2dbfc26
Haris <Harisk96@gmail.com>
2020-06-03 13:39:30 +0200

    Put in parameters for the two animals and added functions and some documentation to superclass.

--- biosim/animals.py
+++ biosim/animals.py
@@ -10,6 +10,54 @@ class Animals:
     Superclass that represents an animal. Contains all the features for for creating the animals.
     herbivores and carnivores are subclasses of this superclass.
     """
+    def __init__(self):
+        """
+        Constructor for animal class.
+        """
+
+    def Set_Params(self):
+        """
+        Setting the parameters given in the project PDF for the animal superclass.
+        :return:
+        """
+
+        w_birth =
+
+    def Inital_Weight(self):
+        """
+         the birth weight is drawn from a Gaussian distribution with wmean_birth and sigma_birth.
+        :return:
+        """
+
+    def Weight_Update(self):
+        """
+        When an animal eats an amount F of fodder, its weight increases by βF. Every year,
+        the weight of the animal decreases by ηw.
+        :return:
+        """
+
+    def Birth(self):
+        """
+        Decides probability for each animal in each cell whether it will give birth or not.
+        Does also provide the conditions that have to be met in order to give birth.
+        :return:
+        """
+
+    def Fitness(self):
+        """
+        The overall condition of the animal is described by its ﬁtness,
+        which is calculated based on age and weight using a formula.
+        :return: Float, value between 0 and 1 representing fitness.
+        """
+
+    def Migrate(self):
+        """
+        Decides the probability that the animal will move to one of the neighbouring four cells.
+        All four cells have equal probability and the animal will not move if the chosen cell
+        is water.
+        :return:
+        """
+
 
 class Herbivore(Animal):
     """
@@ -18,6 +66,24 @@ class Herbivore(Animal):
     lowland as landscape types. It tries to eat an amount F of food each year, but the amount of
     food it can eat depends on the amount of food in the cell. Herbivores eat in a random order.
     """
+
+    w_birth = 8.0
+    sigma_birth = 1.5
+    beta = 0.9
+    eta = 0.05
+    a_half = 40.0
+    phi_age = 0.6
+    w_half = 10.0
+    phi_weight = 0.1
+    mu = 0.25
+    gamma = 0.2
+    zeta = 3.5
+    xi = 1.2
+    omega = 0.4
+    F = 10.0
+    DeltaPhiMax = ???
+
+    def feed(self):
 
 class Carnivore(Animal):
     """
@@ -30,3 +96,26 @@ class Carnivore(Animal):
     The carnivores prey in order of their fitness, so that the fittest carnivore eats first. They
     try to kill one herbivore at a time, trying to kill the herbivore with the lowest fitness first.
     """
+
+    w_birth = 6.0
+    sigma_birth = 1.0
+    beta = 0.75
+    eta = 0.125
+    a_half = 4.0
+    phi_age = 0.3
+    w_half = 4.0
+    phi_weight = 0.4
+    mu = 0.4
+    gamma = 0.8
+    zeta = 3.5
+    xi = 1.1
+    omega = 0.8
+    F = 50.0
+    DeltaPhiMax = 10.0
+
+
+    def slay(self):
+        """
+        Function that determines wheter and when a carnivore will kill a herbivore and consume it.
+        :return:
+        """
