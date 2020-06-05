3bcda541cdcd90b1eba4eec6a812b4312300392b
Haris <Harisk96@gmail.com>
2020-06-05 17:15:44 +0200

    added file simulation

---
+++ biosim/simulation.py
@@ -0,0 +1,10 @@
+
+
+
+from biosim.landscape import Cell, Sea, Desert, Highland, Lowland
+from biosim.animals import Animals, Herbivore, Carnivore
+
+geogr = """\
+               WWW
+               WHW
+               WWW"""
