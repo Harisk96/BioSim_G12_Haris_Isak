d2c5ecc64b7b273e5b45838d4860e742ab145645
Haris <Harisk96@gmail.com>
2020-06-06 16:20:36 +0200

    Added function to landscape to simulate herbivore

--- biosim/landscape.py
+++ biosim/landscape.py
@@ -10,10 +10,27 @@ class Cell:
 
     def __init__(self):
         self.fodder = 0
-        current_animals = {Herbivore: []}
+        current_herbivores = []
+        # current_carnivores = []
+
+    @property
+    def n_herbivores:
+        return self.len(current_herbivores)
+
+    #    @property
+    #    def n_carnivores:
+    #        return self.len(current_carnivores)
+
+    @property
+    def n_animals
+        return self_n_herbivores  # +self.len(current_carnivores)
+
+    def randomise_herbivores(self):
+        # Her må randomisere herbivores listen for å kunne feede listen bortover.
+        random.shuffle(current_herbivores)
 
     def grow_fodder(self):
-
+        self.fodder += 0
 
     def place_animals(self):
         """
@@ -23,34 +40,49 @@ class Cell:
 
     def birth_cycle(self):
 
+        newborn_herbivores = []
+        nr_herbivores = self.n_herbivores
+        if nr_herbivores > 1:
+            for herbivore in self.herbivores:
+                newborn_herbivore = herbivore.animals.birth_check(nr_herbivores)
+                if not newborn:
+                    continue
+                self.herbivores.append(newborn)
+                newborn_herbivores.append(newborn)
+
+                return self.herbivors, newborn_herbivores
 
     def weightloss(self):
+        for herbivore in self.herbivores:
+            herbivore.animals.yearly_weightloss()
 
     def feed(self):
-        #grow fodder
-        #feed_carnivores()
-        #feed_herbivores
+        self.grow_fodder()
+        self.feed_carnivores()
+        self.feed_herbivores
 
-    def feed_herbivore
+    def feed_herbivore(self):
+        biosim.animals.eat
 
-    #def feed carnivore (Jobber vi med senere)
+    # def feed carnivore (Jobber vi med senere)
 
-    def age(self):
+    def age_animals(self):
+        for herbivore in self.herbivores:
+            herbivore.animals.update_age()
 
     def death_square(self):
         surviving = []
         eller_en_liste_med_dode = ?
 
-    def add_newborn_square(self):???
+        def add_newborn_square(self): ?
 
+            ??
 
+        def reset_square(self):
 
+        def delete(self): ?
 
-    def reset_square(self):
-
-
-
-    def delete(self):???
+            ??
 
 class Highland(Cell):
     migrate_to: True
@@ -64,18 +96,18 @@ class Highland(Cell):
         self.fodder = self.params['fmax']
 
 class Lowland(Cell):
-    migrate_to: True
-    params = {'f_max': 800.0}
+            migrate_to: True
+            params = {'f_max': 800.0}
 
     def __init__(self):
         super().__init__()
-        self.fodder = self.params['f_max']
+            self.fodder = self.params['f_max']
 
     def _yearly_fodder(self):
-        self.fodder = self.params['fmax']
+                self.fodder = self.params['fmax']
 
-class Desert(Cell):
-    migrate_to = True
+    lass Desert(Cell):\
+        migrate_to = True
 
-class Sea(Cell):
-    migrate_to = False
+    class Sea(Cell):
+        migrate_to = False
