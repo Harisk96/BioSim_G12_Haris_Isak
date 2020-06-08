7c9682c768aa13865c5c72787836216cd66c86d4
Haris <Harisk96@gmail.com>
2020-06-08 13:46:36 +0200

    Started on island superclass in order to simulate herbivore

---
+++ biosim/island.py
@@ -0,0 +1,17 @@
+from biosim.landscape import Highland, Lowland, Desert, Sea
+from biosim.animals import Herbivore, Carnivore
+
+import numpy as np
+
+def sjekke_at_alle_linjene_med_celle_bokstaver_er_like_før_class_island______Isak_Finnøy_er_lættis(self):
+
+
+class Island:
+
+    cell_types = {'H': Highland,
+                  'L': Lowland,
+                  'D': Desert,
+                  'S': Sea}
+
+    def __init__(self):
+
