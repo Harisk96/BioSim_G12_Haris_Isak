c7cccf9bfafcf8209479d428ae3fb006e89ef09c
Haris <Harisk96@gmail.com>
2020-06-13 14:26:05 +0200

    Added map_checking, surrounded by water and equal length of all mapstrings

--- biosim/island.py
+++ biosim/island.py
@@ -1,12 +1,34 @@
+#LEGGER MAPPET MIDLERTIDIG INN I DENNE FILEN:
+
+geogr = """
+WWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWHWWWWLLLLLLLW
+WHHHHHLLLLWWLLLLLLLWW
+WHHHHHHHHHWWLLLLLLWWW
+WHHHHHLLLLLLLLLLLLWWW
+WHHHHHLLLDDLLLHLLLWWW
+WHHLLLLLDDDLLLHHHHWWW
+WWHHHHLLLDDLLLHWWWWWW
+WHHHLLLLLDDLLLLLLLWWW
+WHHHHLLLLDDLLLLWWWWWW
+WWHHHHLLLLLLLLWWWWWWW
+WWWHHHHLLLLLLLWWWWWWW
+WWWWWWWWWWWWWWWWWWWWW"""
+
+
 from biosim.landscape import Highland, Lowland, Desert, Sea
 from biosim.animals import Herbivore, Carnivore
 
 import numpy as np
 
-def sjekke_at_alle_linjene_med_celle_bokstaver_er_like_for_class_island______Isak_Finnoy_er_laettis(self):
-
+def check_length(strings):
+    strings = list(map(len, strings))
+    if strings.count(strings[0]) == len(strings):
+        return True
 
 class Island:
+
+
 
     cell_types = {'H': Highland,
                   'L': Lowland,
@@ -14,8 +36,32 @@ class Island:
                   'S': Sea}
 
     def __init__(self, letter_map, init_animals):
-        self.map = self.create_map(HER MÅ JEG SETTE INN SELVE MAPPET)
+        self.map = self.create_map(geogr)
         self.place_population(SETTE INN STARTPOPULASJON HER)
+
+    def clean_stringmap(stringmap):
+        stringmap = stringmap.strip()
+        strings = stringmap.split('\n')
+
+        if not check_length(strings):
+            raise ValueError('Every line in stringmap must be of equal length')
+
+        for elems in str([strings[0] + strings[-1]])[2:-2]:
+            if elems != 'W':
+                raise ValueError('Not island, island must be surrounded by water'
+                                 ', error on southside or northside')
+
+        for elems in strings:
+            if not elems.startswith('W'):
+                raise ValueError('Not island, island must be surrounded by water,'
+                                 ' error on westside')
+
+        for elems in strings:
+            if not elems.endswith('W'):
+                raise ValueError('Not island, island must be surrounded by water, '
+                                 'error on eastside')
+
+
 
     def procreate_cells_map(self):
         pass
