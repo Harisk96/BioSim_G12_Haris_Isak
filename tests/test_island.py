6c1a5ebe1a1977bcd9793817c579bf735a612cbe
isakfinnoy <isfi@nmbu.no>
2020-06-16 16:24:44 +0200

    fixed landscape

---
+++ tests/test_island.py
@@ -0,0 +1,7 @@
+from biosim.animals import Herbivore, Carnivore, Animals
+from biosim.landscape import Cell, Lowland, Highland, Desert, Sea
+import pytest
+
+__author__ = 'Haris Karovic', 'Isak Finnøy'
+__email__ = 'harkarov@nmbu.no', 'isfi@nmbu.no'
+
